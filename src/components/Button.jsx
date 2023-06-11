21961228318a9712f3a2c0f04e4ffceb42cfb334
Joseph Essel Moses <jmosesessel21@gmail.com>
2023-06-11 01:15:57 +0000

    update Button and IntroText

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button(props) {
     const text = props.text
   return (
-    <button className={ props.width + ' text-white px-12 h-12 rounded-full bg-gradient-to-tr from-cyan to-blue'}>{text}</button>
+    <button className={ props.width + ' font-bold text-white px-12 h-12 rounded-full bg-gradient-to-tr from-cyan to-blue'}>{text}</button>
   )
 }
 
